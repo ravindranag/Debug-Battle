5596d6ff3736c65351967ee9a9afeccfd94e47aa
SySagar <sysagar07@gmail.com>
2023-02-26 11:01:58 +0530

    testing husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     <h1>Welcome to debug battle!!</h1><div className="main">hello world</div></div>
+      <h1>Welcome to debug battle!!</h1>
+      <div className="main">hello world</div>
+    </div>
   );
 }
 
