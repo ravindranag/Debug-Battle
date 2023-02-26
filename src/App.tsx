2756d88afde2f4b490c410d50e26d9b89be599fe
SySagar <sysagar07@gmail.com>
2023-02-26 10:34:32 +0530

    test 2

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>Welcome to debug battle!!</h1><div className="main">hello world</div>
-    </div>
+     <h1>Welcome to debug battle!!</h1><div className="main">hello world</div></div>
   );
 }
 
